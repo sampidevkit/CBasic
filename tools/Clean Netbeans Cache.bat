79b96022560442be29b77876af0a17528f1239ac
sampidevkit <sampidevkit@gmail.com>
2021-09-11 19:46:42 +0700

    IDE tools

---
+++ tools/Clean Netbeans Cache.bat
@@ -0,0 +1,29 @@
+@echo off
+
+del /f /s /q "C:\Users\sampi\AppData\Local\NetBeans\Cache\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Local\mplab_ide\Cache\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Local\mplab_ipe\Cache\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ide\dev\v5.50\var\cache\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ide\dev\v5.50\var\filehistory\*.*"
+
+::del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ide\*.*"
+
+::del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ipe\*.*"
+
+::del /f /s /q "C:\Users\sampi\AppData\Roaming\NetBeans\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Local\ZaloPC\*.*"
+
+del /f /s /q "C:\Windows\Temp\*.*"
+
+del /f /s /q "C:\Windows\prefetch\*.*"
+
+del /f /s /q "C:\Windows\servicing\LCU\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\Microsoft\teams\Cache"
+
+echo Done.
---
+++ tools/Reset Netbeans.bat
@@ -0,0 +1,9 @@
+@echo off
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ide\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\mplab_ipe\*.*"
+
+del /f /s /q "C:\Users\sampi\AppData\Roaming\NetBeans\*.*"
+
+echo Done.
