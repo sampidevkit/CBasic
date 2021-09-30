0ac40d5c0c820f9f5555c44fe93823330c613e3a
sampidevkit <sampidevkit@gmail.com>
2021-09-30 22:06:16 +0700

    CBasic8

---
+++ ex/CBasic8_fileIO/main.c
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: sampi
+ *
+ * Created on September 30, 2021, 9:03 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
