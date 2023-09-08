389e34b411cde7c09d2c4c3a05b93b0b5021fb81
SAMPIDevKit <sampidevkit@gmail.com>
2023-09-08 23:11:34 +0700

    Update IDE Apache NetBeans 19 released on September 1, 2023

--- ex/CBasic2/nbproject/Makefile-variables.mk
+++ ex/CBasic2/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=cbasic2
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/cbasic2
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/cbasic2
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=cbasic2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/cbasic2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/cbasic2.tar
 # Release configuration
 CND_PLATFORM_Release=MinGW-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
